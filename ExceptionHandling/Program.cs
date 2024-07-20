fb2f3d2e3e241afbf498145dcb45f87e84ef223d
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-20 18:09:28 +0530

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class ExceptionDemo
+{
+    static void Main()
+    {
+
+        try
+        {
+            Console.WriteLine("Enter 1st no");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd no");
+
+            int y = int.Parse(Console.ReadLine());
+
+            int z = x / y;
+            Console.WriteLine("The result is" + z);
+        }
+        catch (DivideByZeroException ex1)
+        {
+            Console.WriteLine(ex1.Message);
+        }
+        catch (FormatException ex2)
+        {
+            Console.WriteLine("Input must be numeric");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.WriteLine("End of the exception program");
+    }
+}
+
